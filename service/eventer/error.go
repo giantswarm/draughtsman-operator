8934f43cd62ab90671be534f69c1c09eb445b374
Tim Schindler <xh3b4sd@gmail.com>
2017-08-31 18:50:34 +0200

    removed unused errors

--- service/eventer/error.go
+++ service/eventer/error.go
@@ -2,8 +2,6 @@ package eventer
 
 import (
 	"github.com/giantswarm/microerror"
-
-	"github.com/giantswarm/draughtsman-operator/service/eventer/github"
 )
 
 var invalidConfigError = microerror.New("invalid config")
@@ -11,11 +9,4 @@ var invalidConfigError = microerror.New("invalid config")
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
-}
-
-var NotFoundError = microerror.New("not found")
-
-// IsNotFound asserts not found errors of eventer implementations.
-func IsNotFound(err error) bool {
-	return microerror.Cause(err) == NotFoundError || github.IsNotFound(err)
 }
